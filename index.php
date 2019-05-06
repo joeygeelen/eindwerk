1d9c01da58a8035b329b9d69922b5d6626deb53c
MartenFelis <marten.felis@hotmail.com>
2019-05-06 20:40:25 +0200

    productbeschrijving toevoegen

---
+++ index.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Marten | Joey</title>
+</head>
+<body>
+    <h1>Project beschrijving</h1>
+    <p>Ons concept is om een webshop voor bier op te bouwen. Gebruikers kunnen bier bestellen uit een breed gamma van verschillende afkomsten. We richten ons niet enkel op de bekende bieren, maar willen ook streekbieren in de kijker zetten. Niet geregistreerde gebruikers kunnen zonder probleem door onze webshop browsen, maar om te bestellen is een registratie wel vereist.</p>
+</body>
+</html>
