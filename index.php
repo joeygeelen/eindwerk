a6a859026f8c1cd5c0712d7d62a9a4d6e936a11a
MartenFelis <marten.felis@hotmail.com>
2019-05-10 11:51:47 +0200

    aanpassen projectbeschrijving

--- index.php
+++ index.php
@@ -9,6 +9,6 @@
 </head>
 <body>
     <h1>Project beschrijving</h1>
-    <p>Ons concept is om een webshop voor bier op te bouwen. Gebruikers kunnen bier bestellen uit een breed gamma van verschillende afkomsten. We richten ons niet enkel op de bekende bieren, maar willen ook streekbieren in de kijker zetten. Niet geregistreerde gebruikers kunnen zonder probleem door onze webshop browsen, maar om te bestellen is een registratie wel vereist.</p>
+    <p>Ons concept is om een webshop voor bier op te bouwen. Gebruikers kunnen bier bestellen uit een breed gamma van verschillende afkomsten. We richten ons niet enkel op de bekende bieren, maar willen ook streekbieren in de kijker zetten. Hierdoor kunnen consumenten kennis maken met streekbieren die ze anders niet tegen zouden komen. Niet geregistreerde gebruikers kunnen zonder probleem door onze webshop browsen, maar om te bestellen is een registratie wel vereist. Een consument kan gemakkelijk zoeken naar bieren doordat we onze bieren filteren per categorie. Zo delen we ze op in kleur, gisting, regio/streek en percentage.</p>
 </body>
 </html>
