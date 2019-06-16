b8c99325e5c96a09bd362020f1456b87341be314
Joey Geelen <joeygeelen@outlook.com>
2019-06-16 11:52:04 +0200

    allebieren api toegevoegd om de bieren te tonen

--- src/app/allebieren/allebieren.component.ts
+++ src/app/allebieren/allebieren.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaderResponse, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-allebieren',
@@ -7,7 +8,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AllebierenComponent implements OnInit {
 
-  constructor() { }
+  title = 'Eindwerk';
+
+  GET_SERVER_URL = 'https://wdev.be/joey/be/restapi/bieren';
+  // DELETE_SERVER_URL = 'http://localhost/nootnoot/user/';
+  // PUT_SERVER_URL = 'http://localhost/nootnoot/user/';
+  // POST_SERVER_URL = 'http://localhost/nootnoot/users';
+  bieren: any;
+  request = new XMLHttpRequest();
+  id;
+  name;
+
+
+  constructor(private http: HttpClient) {
+    // get data when refreshed
+    this.getRequest();
+  }
+
+  // ----- GET -----
+  getRequest() {
+    // get call + responseType = give response as text
+    this.http.get(this.GET_SERVER_URL, {responseType: 'json'})
+        .subscribe((result: any) => {
+          this.bieren = result.results;
+        });
+    // put data in to variable for html-usage
+  }
 
   ngOnInit() {
   }
